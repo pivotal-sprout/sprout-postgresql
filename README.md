cf1b71d0373e828ac45ac77c95c3de63b8882096
Can Berk Güder and Jonathan Barnes <jonathan@pivotallabs.com>
2014-04-10 16:56:26 -0700

    Initial Commit
    includes a basic template for a Chef cookbook

---
+++ README.md
@@ -0,0 +1,21 @@
+# sprout-postgresql cookbook
+Chef Recipes to install and configure PostgreSQL on OSX
+
+# Requirements
+* ruby
+* bundler
+
+# Usage
+
+```
+bundle install
+soloist run_recipe sprout-postgresql
+```
+
+# Attributes
+
+# Recipes
+
+# Author
+* [Jonathan Barnes](http://github.com/codeword)
+* [Can Berk Güder](http://github.com/cbguder)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "sprout-postgresql"
+maintainer       "Pivotal Labs"
+maintainer_email "sprout@pivotallabs.com"
+license          "All rights reserved"
+description      "Recipes to install and configure PostgreSQL on OSX"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "homebrew"
