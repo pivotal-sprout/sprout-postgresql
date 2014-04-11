6d1b69efbc2ad571755d90ef3b04c8e17358aca1
Can Berk Güder and Jonathan Barnes <jonathan@pivotallabs.com>
2014-04-10 18:00:16 -0700

    Adds ability to blow away and recreate db
    [#68384446]

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ node.default['sprout']['postgresql']['plist_filename'] = 'homebrew.mxcl.postgres
 node.default['sprout']['postgresql']['other_plist_filenames_to_unload'] = %w(org.postgresql.postgres.plist)
 node.default['sprout']['postgresql']['create_user_db'] = true
 node.default['sprout']['postgresql']['create_superuser'] = true
+node.default['sprout']['postgresql']['recreate_database_cluster'] = true
---
+++ recipes/recreate_database_cluster.rb
@@ -0,0 +1,14 @@
+recreate_database = node['sprout']['postgresql']['recreate_database_cluster']
+homebrew_root = File.join('/', 'usr', 'local')
+data_directory = File.join(homebrew_root, 'var', 'postgres')
+
+directory data_directory do
+  action :delete
+  recursive true
+  only_if recreate_database
+end
+
+execute 'recreate database cluster' do
+  command "initdb #{data_directory}"
+  only_if recreate_database
+end
