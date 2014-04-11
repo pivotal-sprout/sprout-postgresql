b0928fa0d6b68bb49f9ecaebd57b3e8433997b02
Can Berk Güder and Jonathan Barnes <cbguder@pivotallabs.com>
2014-04-11 11:13:46 -0700

    Refactors destroy to be simlar to recreate [#68384446]
    No functional change

--- recipes/destroy_database_cluster.rb
+++ recipes/destroy_database_cluster.rb
@@ -1,7 +1,9 @@
-opts = node['sprout']['postgresql']
+destroy_database = node['sprout']['postgresql']['destroy_database_cluster']
+homebrew_root = File.join('/', 'usr', 'local')
+data_directory = File.join(homebrew_root, 'var', 'postgres')
 
-directory "/usr/local/var/postgres" do
+directory data_directory do
   action :delete
   recursive true
-  only_if { opts['destroy_database_cluster']}
+  only_if { destroy_database }
 end
