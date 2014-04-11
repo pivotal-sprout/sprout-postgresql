b1e6a41d6a652467c6a76cd50c384c0fc11f6fb5
Can Berk Güder and Jonathan Barnes <jonathan@pivotallabs.com>
2014-04-11 11:01:02 -0700

    Create postgres user [#68384446]

--- recipes/create_postgres_user.rb
+++ recipes/create_postgres_user.rb
@@ -1,5 +1,5 @@
-execute "create the postgres superuser" do
+execute 'create the postgres superuser' do
   command "/usr/local/bin/createuser -U #{node['current_user']} --superuser postgres"
   user node['current_user']
-  only_if { node['sprout']['postgres']['create_superuser'] }
+  only_if { node['sprout']['postgresql']['create_postgres_user'] }
 end
