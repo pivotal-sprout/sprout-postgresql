c1648bece238a5c08c0b03dc7087460457861865
Abhi Hiremagalur and Jonathan Barnes <pair+abhi+jonathan@pivotallabs.com>
2014-04-15 10:15:19 -0700

    Add comment describing intent
    [#61519432]

--- recipes/install_postgres.rb
+++ recipes/install_postgres.rb
@@ -1,8 +1,8 @@
+
 recreate_database = node['sprout']['postgresql']['recreate_database_cluster']
 homebrew_root = File.join('/', 'usr', 'local')
 data_directory = File.join(homebrew_root, 'var', 'postgres')
 preexisting_databases = File.exists?(data_directory)
-env = {'LC_ALL' => nil}
 
 include_recipe "homebrew"
 package "postgresql"
@@ -13,9 +13,10 @@ directory data_directory do
   only_if { !preexisting_databases && recreate_database }
 end
 
-execute 'recreate database cluster' do
+execute 'recreate database cluster' do # Mixlib::ShellOut incorrectly sets LC_ALL
+                                       # to "c" when installing the postgresql Forumala
   command "initdb #{data_directory}"
-  environment env
+  environment 'LC_ALL' => nil
   user node['current_user']
   only_if { !preexisting_databases && recreate_database }
 end
