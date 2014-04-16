5ecbf1317999ae5eb6168ec32bb22e2db124aebc
Abhi Hiremagalur and Jonathan Barnes <abhi@pivotallabs.com>
2014-04-15 17:39:21 -0700

    Backfill spec that actually connects to postgres
    Based on the old postgres recipe [#69556800]

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
+require 'socket'
 
 describe 'sprout-postgresql' do
-  it 'converges' do
+  before :all do
     expect(system('soloist')).to be_true
+  end
+
+  it 'installs and starts postgres' do
+    expect {
+      TCPSocket.open('localhost', 5432).close
+    }.not_to raise_error
+
+    expect {
+      system("sudo -u #{node['current_user']} /usr/local/bin/psql -U postgres < /dev/null")
+    }.to be_true
   end
 end
