78a695f89de87cb413bab3a427dbcba468be8901
Jonathan Barnes <jonathan@pivotallabs.com>
2014-04-15 22:12:54 -0700

    Backfills specs for add_launch_agent recipe [#69556800]

--- recipes/create_current_user_db.rb
+++ recipes/create_current_user_db.rb
@@ -1,6 +1,6 @@
 db_name = node['current_user']
 
-execute 'create the database' do
+execute 'create a database for the current user' do
   command "/usr/local/bin/createdb -U #{db_name}"
   user node['current_user']
   not_if "psql -U #{db_name} -c 'select 1' #{db_name} &> /dev/null"
---
+++ spec/unit/create_current_user_db_spec.rb
@@ -0,0 +1,25 @@
+require 'unit/spec_helper'
+
+describe 'sprout-postgresql::create_current_user_db' do
+  let(:runner) { ChefSpec::Runner.new }
+  let(:existing_db) { false }
+  let(:create_command) { '/usr/local/bin/createdb -U fauxhai' }
+
+  before do
+    stub_command('which git')
+    stub_command(%q(psql -U fauxhai -c 'select 1' fauxhai &> /dev/null)).and_return(existing_db)
+    runner.converge(described_recipe)
+  end
+
+  it 'creates the users database' do
+    expect(runner).to run_execute('/usr/local/bin/createdb -U fauxhai').with(user: 'fauxhai')
+  end
+
+  context 'when the users database aready exists' do
+    let(:existing_db) { true }
+
+    it 'gets skipped' do
+      expect(runner).to_not run_execute('/usr/local/bin/createdb -U fauxhai').with(user: 'fauxhai')
+    end
+  end
+end
