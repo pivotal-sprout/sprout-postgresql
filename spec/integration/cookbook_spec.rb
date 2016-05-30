f905f1f2ccdc54e268a4f77f338cd78242c09fc1
Dan Wendorf <dwendorf@pivotal.io>
2016-05-30 16:12:11 -0700

    Don't use RSpec's old be_true syntax

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -4,7 +4,7 @@ require 'socket'
 RSpec.describe 'sprout-postgresql' do
   before :all do
     system('psql -c "select 1" &> /dev/null') && raise('This system already has postgres installed')
-    expect(system('soloist')).to be_true
+    expect(system('soloist')).to eq(true)
   end
 
   it 'runs postgres on the default port' do
@@ -12,7 +12,7 @@ RSpec.describe 'sprout-postgresql' do
       TCPSocket.open('localhost', 5432).close
     end.not_to raise_error
 
-    expect(system('psql -c "select 1" &> /dev/null')).to be_true
+    expect(system('psql -c "select 1" &> /dev/null')).to eq(true)
   end
 
   it 'creates the databases with a UTF-8 encoding' do
@@ -20,15 +20,15 @@ RSpec.describe 'sprout-postgresql' do
   end
 
   it 'is managed by launchd' do
-    expect(system('launchctl list homebrew.mxcl.postgresql &> /dev/null')).to be_true
+    expect(system('launchctl list homebrew.mxcl.postgresql &> /dev/null')).to eq(true)
   end
 
   it 'creates a database for the current user' do
     db_name = ENV['USER']
-    expect(system("psql -U #{db_name} -c 'select 1' #{db_name} &> /dev/null")).to be_true
+    expect(system("psql -U #{db_name} -c 'select 1' #{db_name} &> /dev/null")).to eq(true)
   end
 
   it 'creates a postgres user' do
-    expect(system('psql -U postgres -c "select 1" &> /dev/null')).to be_true
+    expect(system('psql -U postgres -c "select 1" &> /dev/null')).to eq(true)
   end
 end
