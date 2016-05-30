2e4e3f7903cd5c5adae36879578d72403db7f39a
Dan Wendorf <dwendorf@pivotal.io>
2016-05-30 16:29:17 -0700

    Use `brew services` to start postgresql
    We don't need a custom script now that brew services exists

--- recipes/add_launch_agent.rb
+++ recipes/add_launch_agent.rb
@@ -1,24 +1,7 @@
 include_recipe 'sprout-postgresql::install_postgres'
 
-launch_agents_path = File.expand_path(File.join(node.default['sprout']['home'], 'Library/LaunchAgents'))
-directory launch_agents_path do
-  action :create
-  recursive true
-  owner node['sprout']['user']
-end
-
-plist_filename = node['sprout']['postgresql']['plist_filename']
-homebrew_root = '/usr/local'
-postgresql_install_path = File.join(homebrew_root, 'opt', 'postgresql')
-source_plist_filename = File.join(postgresql_install_path, plist_filename)
-launch_agent_plist_filename = File.join(launch_agents_path, plist_filename)
-
-link launch_agent_plist_filename do
-  to source_plist_filename
-end
-
-execute 'start the daemon' do
-  command "launchctl load -w #{launch_agent_plist_filename}"
+execute 'start postgresql' do
+  command 'brew services start postgresql'
   user node['sprout']['user']
 end
 
--- spec/unit/add_launch_agent_spec.rb
+++ spec/unit/add_launch_agent_spec.rb
@@ -2,8 +2,6 @@ require 'unit/spec_helper'
 
 RSpec.describe 'sprout-postgresql::add_launch_agent' do
   let(:runner) { ChefSpec::SoloRunner.new }
-  let(:plist_filename) { runner.node['sprout']['postgresql']['plist_filename'] }
-  let(:launchagent_path) { '/home/fauxhai/Library/LaunchAgents' }
 
   before do
     stub_command('which git')
@@ -14,19 +12,9 @@ RSpec.describe 'sprout-postgresql::add_launch_agent' do
     expect(runner).to include_recipe('sprout-postgresql::install_postgres')
   end
 
-  it 'ensures the launchAgents directory exists' do
-    expect(runner).to create_directory(launchagent_path).with(recursive: true).with(user: 'fauxhai')
-  end
-
-  it 'symlinks the homebrew launch configuration into the launchAgents folder' do
-    expect(runner).to create_link(
-      "#{launchagent_path}/#{plist_filename}"
-    ).with(to: "/usr/local/opt/postgresql/#{plist_filename}").with(link_type: :symbolic)
-  end
-
-  it 'launches postgres' do
+  it 'launches postgres with brew services' do
     expect(runner).to run_execute(
-      "launchctl load -w #{launchagent_path}/#{plist_filename}"
+      'brew services start postgresql'
     ).with(user: 'fauxhai')
   end
 end
