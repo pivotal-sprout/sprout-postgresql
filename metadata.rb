cc5aab60a470f738a8cbe14243756878912a6f3a
Dan Wendorf <dwendorf@pivotal.io>
2016-05-30 15:36:11 -0700

    Address foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,6 @@ version          '0.1.0'
 supports         'mac_os_x'
 depends          'homebrew', '>= 1.5.4'
 depends          'sprout-base'
+
+issues_url 'https://github.com/pivotal-sprout/sprout-postgresql/issues'
+source_url 'https://github.com/pivotal-sprout/sprout-postgresql'
